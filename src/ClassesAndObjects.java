074ab1d72901ae28563fafc2bf435882d66af638
praveen reddy nagilla <praveenreddynagilla1996@gmail.com>
2023-10-14 11:24:14 +0530

    new file

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,2 +1,9 @@
-public class ClassesAndObjects {
+ public class ClassesAndObjects {
+     int number;
+     String name;
+
+     public void printDetails(int number,String name){
+          this.number=number;
+          this.name=name;
+     }
 }
